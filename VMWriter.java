d111006e4bfdccc50506fc1667f259d4a97be7e3
GEORGE MATTA <jojomedhat2004@gmail.com>
2023-11-29 15:53:04 -0800

    fill vmwriter

--- VMWriter.java
+++ VMWriter.java
@@ -29,39 +29,51 @@ public class VMWriter {
     }
 
     public void writePush(Segment segment, int index){
-
+        writeCommand("push", segment.toString(), String.valueOf(index));
     }
 
     public void writePop(Segment segment, int index){
-
+        writeCommand("pop", segment.toString(), String.valueOf(index));
     }
 
     public void writeArithmetic(Command command){
-
+        writeCommand(command.toString());
     }
 
     public void writeLabel(String label){
-
+        writeCommand("label", label);
     }
 
     public void writeGoto(String label){
-
+        writeCommand("goto", label);
     }
 
     public void writeIf(String label){
-
+        writeCommand("if-goto", label);
     }
 
     public void writeCall(String name, int numArgs){
-
+        writeCommand("call", name, String.valueOf(numArgs));
     }
 
     public void writeFunction(String name, int numLocals){
-
+        writeCommand("function", name, String.valueOf(numLocals));
     }
 
     public void writeReturn(){
+        writeCommand("return");
+    }
 
+    public void writeCommand(String command){
+        writeCommand(command, "", "");
+    }
+
+    public void writeCommand(String command, String argument){
+        writeCommand(command, argument, "");
+    }
+
+    public void writeCommand(String command, String arg1, String arg2){
+        writer.write(command + " " + arg1 + " " + arg2 + "\n");
     }
 
     public void close(){
@@ -72,10 +84,39 @@ public class VMWriter {
 enum Segment{
     CONST, ARG, LOCAL,
     STATIC, THIS, THAT, POINTER, TEMP;
+
+    @Override
+    public String toString(){
+        switch(this.name()){
+            case "CONST":
+                return "constant";
+            case "ARG":
+                return "argument";
+            case "LOCAL":
+                return "local";
+            case "STATIC":
+                return "static";
+            case "THIS":
+                return "this";
+            case "THAT":
+                return "that";
+            case "POINTER":
+                return "pointer";
+            case "TEMP":
+                return "temp";
+            default:
+                throw new RuntimeException("Unknown Segment " + this.name());
+        }
+    }
 }
 
 enum Command{
     ADD, SUB, NEG,
     EQ, GT, LT,
     AND, OR, NOT;
+
+    @Override
+    public String toString(){
+        return this.name().toLowerCase();
+    }
 }
